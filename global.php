2a9567833aaa294e2cceb158da18f892d6ac1e54
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-07-14 18:10:02 -0400

    Moved global.php to parent project

--- global.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(__DIR__ . '/config.php');
-require_once(__DIR__ . '/users/users.php');
-
-require_once(__DIR__ . '/php-bootstrap/bootstrap.php');
-
-$project_env = PHPBootstrap\bootstrap(__FILE__);
